dfb7a4b8249f07885e6e8cf28067ea01de163a7a
Egor <felbushe@gmail.com>
2025-10-19 18:45:04 +0500

    Практикуемся в работе с HTTPX

--- httpx_update_user.py
+++ httpx_update_user.py
@@ -15,6 +15,7 @@ create_user_response = httpx.post("http://localhost:8000/api/v1/users",
                                   json=create_user_payload)
 create_user_response_data = create_user_response.json()
 print('Create user data:', create_user_response_data)
+print("Status Code:", create_user_response.status_code)
 
 # Проходим аутентификацию
 login_payload = {
@@ -25,6 +26,7 @@ login_response = httpx. post("http://localhost:8000/api/v1/authentication/login"
                              json=login_payload)
 login_response_data = login_response.json()
 print('Login data:', login_response_data)
+print("Status Code:", login_response.status_code)
 
 patch_user_payload = {
     "email": get_random_email(),
