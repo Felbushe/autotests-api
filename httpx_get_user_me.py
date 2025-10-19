8bb4a75c2e32a30704451e87c009b571a16301c0
Egor <felbushe@gmail.com>
2025-10-19 15:12:22 +0500

    Введение в HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -11,27 +11,13 @@ login_response_data_user = login_response_user.json()
 print("Login response user:", login_response_data_user)
 print("Status Code:", login_response_user.status_code)
 
-refresh_payload = {
-    "refreshToken": login_response_data_user["token"]["refreshToken"]
+
+get_users_headers = {
+    "Authorization": f"Bearer {login_response_data_user['token']['accessToken']}"
 }
 
-# Выполняем запрос на обновление токена
-refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json=refresh_payload)
-refresh_response_data = refresh_response.json()
-
-# Выводим обновленные токены
-print("Refresh response:", refresh_response_data)
-print("Status Code:", refresh_response.status_code)
-
-access_token = {
-        login_response_data_user ['token']['accessToken']
-}
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-
-users_me = httpx.get("http://localhost:8000/api/v1/users/me", headers=headers)
+users_me = httpx.get("http://localhost:8000/api/v1/users/me", headers=get_users_headers)
 users_me_data = users_me.json()
 
-print("Info:", users_me_data)
+print("Get user response:", users_me_data)
 print("Status Code:", users_me.status_code)
